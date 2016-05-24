fa5c41523f252f30999559ae89d45dcf03d1cef1
ccoenraets <ccoenraets@gmail.com>
2016-05-23 23:35:10 -0400

    step3

--- js/app.js
+++ js/app.js
@@ -1,54 +1,57 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-var calculatePayment = function(principal, years, rate) {
-    var monthlyRate = rate / 100 / 12;
-    var monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1/(1 + monthlyRate), years * 12)));
-    var balance = principal;
-    var amortization = [];
-    for (var y=0; y<years; y++) {
-        var interestY = 0;  //Interest payment for year y
-        var principalY = 0; //Principal payment for year y
-        for (var m=0; m<12; m++) {
-            var interestM = balance * monthlyRate;       //Interest payment for month m
-            var principalM = monthlyPayment - interestM; //Principal payment for month m
+let calculatePayment = (principal, years, rate) => {
+    let monthlyRate = rate / 100 / 12;
+    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1/(1 + monthlyRate), years * 12)));
+    let balance = principal;
+    let amortization = [];
+    for (let y=0; y<years; y++) {
+        let interestY = 0;  //Interest payment for year y
+        let principalY = 0; //Principal payment for year y
+        for (let m=0; m<12; m++) {
+            let interestM = balance * monthlyRate;       //Interest payment for month m
+            let principalM = monthlyPayment - interestM; //Principal payment for month m
             interestY = interestY + interestM;
             principalY = principalY + principalM;
             balance = balance - principalM;
         }
-        amortization.push({principalY: principalY, interestY: interestY, balance: balance});
+        // ES6: Object creation shorthand
+        amortization.push({principalY, interestY, balance});
     }
-    return {monthlyPayment: monthlyPayment, amortization:amortization};
+    return {monthlyPayment, amortization};
 };
 
-var Header = React.createClass({
-    render: function () {
+class Header extends React.Component {
+
+    // ES6: Arrow function shorthand when function consists of single line return statement
+    render() {
         return (
             <header>
                 <h1>{this.props.title}</h1>
             </header>
-        );
+        )
     }
-});
 
-var AmortizationChart = React.createClass({
-    render: function () {
-        var items = this.props.data.map(function (year, index) {
-            return (
-                <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td className="currency principal">{Math.round(year.principalY).toLocaleString()}</td>
-                    <td className="stretch">
-                        <div className="flex">
-                            <div className="bar principal" style={{flex: year.principalY, WebkitFlex: year.principalY}}></div>
-                            <div className="bar interest" style={{flex: year.interestY, WebkitFlex: year.interestY}}></div>
-                        </div>
-                    </td>
-                    <td className="currency interest">{Math.round(year.interestY).toLocaleString()}</td>
-                    <td className="currency">{Math.round(year.balance).toLocaleString()}</td>
-                </tr>
-            );
-        });
+};
+
+class AmortizationChart extends React.Component {
+    render() {
+        // ES6: Arrow function shorthand when function consists of single line return statement
+        let items = this.props.data.map((year, index) =>
+            <tr key={index}>
+                <td>{index + 1}</td>
+                <td className="currency principal">{Math.round(year.principalY).toLocaleString()}</td>
+                <td className="stretch">
+                    <div className="flex">
+                        <div className="bar principal" style={{flex: year.principalY, WebkitFlex: year.principalY}}></div>
+                        <div className="bar interest" style={{flex: year.interestY, WebkitFlex: year.interestY}}></div>
+                    </div>
+                </td>
+                <td className="currency interest">{Math.round(year.interestY).toLocaleString()}</td>
+                <td className="currency">{Math.round(year.balance).toLocaleString()}</td>
+            </tr>
+        );
         return (
             <table>
                 <thead>
@@ -64,43 +67,48 @@ var AmortizationChart = React.createClass({
             </table>
         );
     }
-});
+};
 
-var MortgageCalculator = React.createClass({
-    getInitialState: function() {
-        return {
+class MortgageCalculator extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
             principal: this.props.principal,
             years: this.props.years,
             rate: this.props.rate
         };
-    },
-    principalChange: function(event) {
+    }
+
+    principalChange(event) {
         this.setState({principal: event.target.value});
-    },
-    yearsChange: function(event) {
+    }
+
+    yearsChange(event) {
         this.setState({years: event.target.value});
-    },
-    rateChange: function(event) {
+    }
+
+    rateChange(event) {
         this.setState({rate: event.target.value});
-    },
-    render: function () {
-        var payment = calculatePayment(this.state.principal, this.state.years, this.state.rate);
-        var monthlyPayment = payment.monthlyPayment;
-        var amortization = payment.amortization;
+    }
+
+    render() {
+        // ES6: Object destructuring syntax
+        let {monthlyPayment, amortization} = calculatePayment(this.state.principal, this.state.years, this.state.rate);
         return (
             <div className="content">
                 <div className="form">
                     <div>
                         <label>Principal:</label>
-                        <input type="text" value={this.state.principal} onChange={this.principalChange}/>
+                        <input type="text" value={this.state.principal} onChange={this.principalChange.bind(this)}/>
                     </div>
                     <div>
                         <label>Years:</label>
-                        <input type="text" value={this.state.years} onChange={this.yearsChange}/>
+                        <input type="text" value={this.state.years} onChange={this.yearsChange.bind(this)}/>
                     </div>
                     <div>
                         <label htmlFor="rate">Rate:</label>
-                        <input type="text" value={this.state.rate} onChange={this.rateChange}/>
+                        <input type="text" value={this.state.rate} onChange={this.rateChange.bind(this)}/>
                     </div>
                 </div>
                 <h2>Monthly Payment: <span className="currency">{Number(monthlyPayment.toFixed(2)).toLocaleString()}</span></h2>
@@ -108,17 +116,19 @@ var MortgageCalculator = React.createClass({
             </div>
         );
     }
-});
+};
 
-var App = React.createClass({
-    render: function () {
+class App extends React.Component {
+
+    render() {
         return (
             <div>
-                <Header title="React Mortgage Calculator"/>
+                <Header title="React ES6 Mortgage Calculator"/>
                 <MortgageCalculator principal="200000" years="30" rate="5"/>
             </div>
         );
     }
-});
 
-ReactDOM.render(<App/>,  document.getElementById("app"));
+};
+
+ReactDOM.render(<App/>, document.getElementById("app"));
